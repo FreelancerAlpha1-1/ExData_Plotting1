508f5087aff5b8f0b863a0fccd28f96dcf533ff7
FreelancerAlpha1-1 <jakobkoziel@gmail.com>
2014-06-07 11:33:41 -0600

    Code for Creating Plot 1
    This is the code for not only reading the data, so that Plot 1 can be reproduced, but also for creating the PNG file.

---
+++ Plot1.R
@@ -0,0 +1,23 @@
+Plot 1 
+
+## Set the Working Directory
+setwd("C://mydocuments/ExData_Plotting1")
+
+## Read the txt file into R
+data <- read.csv("household_power_consumption.txt", na.strings="?", sep=";")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+## Set the Dates To Be Between 2007-02-01 and 2007-02-02.
+start.time <- as.Date("01/02/2007", format = "%d/%m/%Y")
+end.time <- as.Date("02/02/2007", format = "%d/%m/%Y")
+
+png(filename="plot1.png")
+
+## Making Plot 1 
+hist(data$Global_active_power[data$Date == start.time | data$Date == end.time],
+     breaks = 12,
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+
+dev.off()
